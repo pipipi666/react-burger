d02e1114573edb48dfc329b31f1aabd6c6161d0d
pipipi666 <nnm107072@gmail.com>
2022-10-12 23:16:31 +0300

    store ts

--- src/services/store.ts
+++ src/services/store.ts
@@ -17,5 +17,5 @@ export type AppDispatch = typeof store.dispatch
 //export type RootReducer = typeof ingredientsReducer | typeof authReducer
 export type RootState = ReturnType<typeof store.getState>
 export type TAppActions = TIngredientsActions | TAuthActions
-export type AppThunk = ThunkAction<void, RootState, unknown, Action<TAppActions>>
-export type Dispatch = <TReturnType = void>(action: TAppActions) => TReturnType;
+export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootState, unknown, TAppActions>
+export type Dispatch = <TReturnType>(action: TAppActions | AppThunk) => TReturnType;
